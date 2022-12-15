252fa1e67ec7b27669084503c3b1f28be85fcb56
Y-v-a-i-n <107935034+Y-v-a-i-n@users.noreply.github.com>
2022-12-15 23:49:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# A simple Eval Deobfuscator in JavaScript that I made just for fun and out of boredom 
+### Just replace eval with console.log and execute the code to get the content (WARNING!! CODE CAN BE EXECUTED IN A CONSOLE.LOG EXAMPLE : `console.log(console.log("Hello World"))` YOU MUST THEREFORE EXECUTE THESE CODES IN A SECURE ENVIRONMENT)
+### How to launch Eval Deobfuscator : 
+* you must first install electronJs on your machine
+```sh
+$ npm install electron -g
+```
+* then you have to run the program with electron, for that you can run this command at the location of the index.js file, you can also run run.bat or run.sh
+```sh
+$ electron index.js
+```
+![image](https://cdn.discordapp.com/attachments/1025769112221270050/1053077296761798696/image.png)
---
+++ index.js
@@ -0,0 +1,26 @@
+const {
+    app: app,
+    BrowserWindow: BrowserWindow
+} = require("electron");
+const createWindow = () => {
+    let window = new BrowserWindow({
+        titleBarStyle: "defaut",
+        title: "JSDefender Deobfuscator",
+        frame: !0,
+        width: 1e3,
+        height: 550,
+        icon: __dirname + "\\img\\logo_javascript-300x300-2370592634.png",
+        show: !0,
+        autoHideMenuBar: !0,
+        resizable: !1,
+    });
+    window.loadFile("src/index.html")
+    window.on("closed", (() => {
+        window = null
+    }))
+};
+app.on("ready", createWindow), app.on("window-all-closed", (() => {
+    if ("darwin" !== process.platform) return app.quit()
+})), app.on("activate", (() => {
+    if (null === win) return createWindow()
+}));
